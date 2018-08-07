270f943ec9dcf9bf616f7e9b863a20844277e0bc
Derek Rush <drush81@gmail.com>
2018-08-07 16:50:32 -0700

    Include prod.secret.exs and use environment variables

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Stockex.Mixfile do
     [
       app: :stockex,
       version: "0.0.1",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       build_embedded: Mix.env == :prod,
